f2d7c84e7675e8a4e7b839ab7aba8ca48edba935
Dmitriy Pavlov <dpavlov@apache.org>
2021-02-21 19:29:03 +0300

    Stub code to start web

---
+++ rsync-srv/src/main/java/org/dpavlov/RsyncApp.java
@@ -0,0 +1,11 @@
+package org.dpavlov;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RsyncApp {
+    public static void main(String[] args) {
+        SpringApplication.run(RsyncApp.class, args);
+    }
+}
